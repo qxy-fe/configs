e3513c5617433eeef3c436b481c740f0683a805c
ntnyq <ntnyq@foxmail.com>
2020-11-13 20:09:20 +0800

    fix: vue-multiline-attr

--- packages/eslint/vue/lib/index.js
+++ packages/eslint/vue/lib/index.js
@@ -27,6 +27,28 @@ module.exports = {
     'vue/no-v-html': 'off',
 
     /**
+     * Require a line break before and after the contents of a multiline element
+     *
+     * @see https://eslint.vuejs.org/rules/multiline-html-element-content-newline.html
+     */
+    'vue/multiline-html-element-content-newline': ['error', {
+      ignoreWhenEmpty: true,
+      ignores: [],
+      allowEmptyLines: false,
+    }],
+
+    /**
+     * Require a line break before and after the contents of a singleline element
+     *
+     * @see https://eslint.vuejs.org/rules/singleline-html-element-content-newline.html
+     */
+    'vue/singleline-html-element-content-newline': ['error', {
+      ignoreWhenNoAttributes: true,
+      ignoreWhenEmpty: true,
+      ignores: [],
+    }],
+
+    /**
      * Disallow usage of this in template
      *
      * @see https://eslint.vuejs.org/rules/this-in-template.html
