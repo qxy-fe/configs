fa989f219115b78b79c0fdfbc42031bf2dc4c8ba
ntnyq <ntnyq@foxmail.com>
2021-01-15 19:09:05 +0800

    feat: eslint-config-jest

--- packages/eslint-config-jest/lib/index.js
+++ packages/eslint-config-jest/lib/index.js
@@ -1,5 +1,6 @@
 /**
  * Eslint config for Jest
+ * @see https://github.com/jest-community/eslint-plugin-jest
  */
 
 module.exports = {
@@ -22,8 +23,20 @@ module.exports = {
         createModuleStore: false,
       },
 
+      plugins: [
+        'jest',
+      ],
+
       rules: {
+        // Disabled because jest.mock should go before module import
         'import/first': 'off',
+
+        // Jest rules
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
       },
     },
   ],
