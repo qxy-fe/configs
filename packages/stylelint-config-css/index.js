3eb23500a927d73dd3fd460ed105ab7a5a9901b6
ntnyq <ntnyq13@gmail.com>
2023-02-10 13:06:16 +0800

    feat: upgrade stylelint to v15

--- packages/stylelint-config-css/index.js
+++ packages/stylelint-config-css/index.js
@@ -17,36 +17,21 @@ module.exports = {
     'output',
     'coverage',
     'public',
-    'static',
-    'temp',
   ],
 
   rules: {
-    // Specify double quotes around strings
-    'string-quotes': 'double',
-
     // Specify lowercase or uppercase for keywords values
     'value-keyword-case': ['lower', {
       ignoreProperties: [/^\$?font/],
     }],
 
+    // Specify string or URL notation for `@import` rules
+    'import-notation': null,
+
     // Disallow selectors of lower specificity from coming after overriding selectors of higher specificity
     'no-descending-specificity': [true, {
       ignore: ['selectors-within-list'],
     }],
-
-    // Require a newline or disallow whitespace after the colon of declarations
-    'declaration-colon-newline-after': 'always-multi-line',
-
-    // Disallow shorthand properties that override related longhand properties
-    // TODO: ignore background for compatible
-    'declaration-block-no-shorthand-property-overrides': null,
-
-    // Specify string or URL notation for `@import` rules
-    'import-notation': null,
-
-    // Require a leading zero for fractional numbers less than 1
-    'number-leading-zero': 'always',
 
     // Specify percentage or number notation for alpha-values
     'alpha-value-notation': 'number',
@@ -54,17 +39,28 @@ module.exports = {
     // Specify modern or legacy notation for applicable color-functions
     'color-function-notation': 'legacy',
 
-    // Require quotes for attribute values
-    'selector-attribute-quotes': 'always',
+    /**
+     * Declaration block
+     */
+
+    // Disallow shorthand properties that override related longhand properties
+    // TODO: ignore background for compatible
+    'declaration-block-no-shorthand-property-overrides': null,
+
+    /**
+     * Selector
+     */
 
     // Specify single or double colon notation for applicable pseudo-elements
     'selector-pseudo-element-colon-notation': 'single',
-
-    // Specify a pattern for class selectors
+    // Specify a pattern for id selectors #snake_case
+    'selector-id-pattern': '^([a-z][a-z0-9]*)(_[a-z0-9]+)*$',
+    // Specify a pattern for class selectors #kebab-case
     'selector-class-pattern': '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
 
-    // Specify a pattern for id selectors
-    'selector-id-pattern': '^([a-z][a-z0-9]*)(_[a-z0-9]+)*$',
+    /**
+     * At-rule
+     */
 
     // Require or disallow an empty line before at-rules
     'at-rule-empty-line-before': ['always', {
